9f93117941be4885cf471b4575d6e4b01b868b80
tatachen <tata9876@gmail.com>
2019-08-09 11:48:06 +0800

    Initial Commit

---
+++ Homework2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework2
+//
+//  Created by Tata on 2019/8/9.
+//  Copyright © 2019 Tata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
